ff94c1f5622b60eadd55db444d1b7f39be6213a9
blabaret <dennis.xiayue@kellogg.northwestern.edu>
2023-03-10 17:56:38 +0000

    Can see places/show even if not logged in

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"]})
     @user = User.find_by({ "email" => params["email"] })
     @sessionid = session["user_id"]
   end
